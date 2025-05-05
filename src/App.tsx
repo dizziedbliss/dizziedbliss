9727b2859a9368b67134b252abe0a18584a50585
dizziedbliss <avengeraditya183@gmail.com>
2025-05-06 00:08:03 +0530

    Initializing and completed static comps - Home and 50% of Projects

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles/components/App.scss";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+
+function App() {
+    return (
+        <>
+            <Home />
+            <Projects />
+        </>
+    );
+}
+
+export default App;
